b910b0ce32d7d97d3c42d8b8ecf80a52b5909111
angcapstep1980 <angcapstep1980@gmail.com>
2024-03-22 16:54:21 +0100

    aggiunto test unitario

---
+++ test_unitario.js
@@ -0,0 +1,15 @@
+function dif(a, b) {
+  return a - b;
+}
+
+function testDif() {
+  const result = dif(3, 2);
+  const expected = 1;
+  if (result === expected) {
+    console.log('Test passed');
+  } else {
+    console.error('Test failed. Expected', expected, 'but got', result);
+  }
+}
+
+testDif();
